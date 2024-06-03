9387dc34c7d32211418db795bdb07ac1d33c0f1b
TLiLi HAMDI <tlilios@yahoo.com>
2024-06-03 02:07:22 +0200

    StartAPI

---
+++ HackNews/HackNews/HackNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsApp.swift
+//  HackNews
+//
+//  Created by HTLILI on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
