4a358a16cddaf08bb480fa1831c1c49fb706d05d
TLiLi HAMDI <tlilios@yahoo.com>
2024-06-04 17:15:14 +0200

    OnGoing

--- HackNews/HackNews/ContentView.swift
+++ HackNews/HackNews/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Text("Morning")
+                Text("Hello, world!")
+            }
         }
         .padding()
     }
